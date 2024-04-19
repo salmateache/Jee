53f0ba846fd7bf56a4a4cdedd924471914dd97f0
salmateache <salmateache0@gmail.com>
2024-04-19 18:06:58 +0100

    sign up page et connection et homepage

---
+++ src/com/database/DB.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.database;
+import com.sun.jdi.connect.spi.Connection;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+
+public class DB {
+    public static Connection con = null;
+    public static void Loadconnection(){
+        String url = "jdbc:mysql://localhost:3306/jeeprojet";
+        String user = "root";
+        String mdp = "";
+        try{
+            con = (Connection) DriverManager.getConnection(url ,user ,mdp);
+            if(con != null){
+                  JOptionPane.showMessageDialog(null,"erreur");   
+            }
+        }
+        catch (Exception e ){
+            JOptionPane.showMessageDialog(null,"erreur"+e);
+                    }
+    }
+}
